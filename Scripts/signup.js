71420b86288d7ea14daf9e1b591d68d374f8b049
Ritesh <kothawaderitesh2010@gmail.com>
2022-12-19 09:14:28 +0530

    code push successfully

--- Scripts/signup.js
+++ Scripts/signup.js
@@ -13,7 +13,8 @@ let form = document.getElementById("form");
         LSData = [];
       }
       LSData.push(obj);
-      localStorage.setItem("accountList",JSON.stringify(LSData))
+      localStorage.setItem("accountList",JSON.stringify(LSData));
+      alert("Account Created Successfully")
     })
 // signup storing data ends here
 
@@ -22,45 +23,3 @@ let form = document.getElementById("form");
 
 
 
-
-// function storeData(){
-//     let email = document.getElementById("email");
-//     let password = document.getElementById("password");
-//     let lowerCaseLetters = /[a-z]/g;
-//     let upperCaseLetters = /[A-Z]/g;
-//     let numbers = /[0-9]/g;
-
-//     let myForm = document.querySelector("form");
-
-//     myForm.addEventListener("submit", function(event) {
-//         event.preventDefault();
-//     let obj = {
-//             email:myForm.email.value,
-//             password:myForm.password.value,
-//         }
-
-//     let users = [];
-//     users.push(obj)
-//     console.log(users);
-
-//         if(email.value.length == 0){
-//             alert("Please fill the email");
-//         } else if(password.value.length == 0){
-//             alert("Please fill the password");
-//         }else if(email.value.length == 0 && password.value.length == 0){
-//             alert('Please fill in email and password');
-//         }else if(password.value.length < 8){
-//             alert('Min of 8 characters required');
-//         }else if(!password.value.match(numbers)){
-//             alert('please add 1 number');
-//         }else if(!password.value.match(upperCaseLetters)){
-//             alert('please add 1 uppercase letter');
-//         }else if(!password.value.match(lowerCaseLetters)){
-//             alert('please add 1 lowercase letter');
-//         }else{
-//             localStorage.setItem('users', JSON.stringify(users));
-        
-//             alert('Your account has been created');
-//         }
-//     })
-// }
